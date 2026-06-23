3562ca1fab7a59082f64d1bc0daa22915bd83c69
Syam Joshi Kalaga <syamjoshik@gmail.com>
2026-06-23 12:19:30 +0530

    Change health endpoint to /books/health

--- book/book_service.py
+++ book/book_service.py
@@ -1,13 +1,10 @@
-
 from flask import Flask, jsonify
 import mysql.connector
 import os
 import logging
 
 app = Flask(__name__)
-
 logging.basicConfig(level=logging.INFO)
-
 
 def get_db():
     return mysql.connector.connect(
@@ -17,34 +14,23 @@ def get_db():
         database=os.getenv("DB_NAME")
     )
 
-
-@app.route("/health", methods=["GET"])
+@app.route("/books/health", methods=["GET"])
 def health():
     return jsonify({"status": "healthy"}), 200
-
 
 @app.route("/books", methods=["GET"])
 def get_books():
     try:
         conn = get_db()
-
         cursor = conn.cursor(dictionary=True)
-
         cursor.execute("SELECT * FROM books")
-
         books = cursor.fetchall()
-
         cursor.close()
         conn.close()
-
         return jsonify(books)
-
     except Exception as e:
         logging.error(str(e))
         return jsonify({"error": str(e)}), 500
 
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
-
-
